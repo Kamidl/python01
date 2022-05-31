8cbb2f3767a37c1ef44d1eee5e744f82caebdac4
Lenovo <zhenglongdai@163.com>
2022-05-31 14:49:08 +0800

    Initial commit message

---
+++ qwe.py
@@ -0,0 +1,75 @@
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+
+
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 1测试登录的用例
+
+
+# case1:输入争取的用户和正确的密码进行登录
+
+# def test_login_success(self):
+#     except_value = 0
+#     actual_value = login('admin','123456').get('code')
+#     self.asserEqual(except_value,actual_value)
+#
+#
+# # case2 :输入错误的用户名或密码登录
+#
+# def test_user_wrong(self):
+#     except_value = 1
+#     actual_value = login('bca','12345')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
